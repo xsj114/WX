9e80d6b74962d38f3dd5f0a6004361bfea26ddb1
xushijie <15101587790@163.com>
2019-03-10 02:49:09 +0800

    firstCommit

---
+++ data/posts-data.js
@@ -0,0 +1,61 @@
+var local_database = [
+  {
+    date: "Sep 18 2016",
+    title: "正是虾肥蟹壮时",
+    imgSrc: "/images/post/crab.png",
+    avatar: "/images/avatar/1.png",
+    img_condition: true,
+    content: "看见很多风景都，是风华绝代放寒假快点放假，回家发的积分兑换积分的活但双方恢复对话，动仕杰方法见风使舵",
+    reading: "112",
+    collection: "96",
+    postId : 0
+  },
+  {
+    date: "Nov 21 2016",
+    title: "比例点击就看到",
+    imgSrc: "/images/post/bl.png",
+    avatar: "/images/avatar/1.png",
+    img_condition: true,
+    content: "诶入耳入额为为溽热UI为日俄乳热舞未入耳呜诶瑞热舞瑞沃尔UI一二的环境而",
+    reading: "11",
+    collection: "6",
+    postId: 1
+  },
+  {
+    date: "Sep 21 2016",
+    title: "货到付款的发挥地方",
+    imgSrc: "/images/post/cat.png",
+    avatar: "/images/avatar/1.png",
+    img_condition: true,
+    content: "江东父老就否定了记得发酒疯的发动机江东父老就否定了记得发酒疯的发动机江东父老就否定了记得发酒疯的发动机",
+    reading: "121",
+    collection: "699",
+    postId: 2
+  },
+  {
+    date: "Oct 11 2016",
+    title: "sls的微信",
+    imgSrc: "/images/post/sls.jpg",
+    avatar: "/images/avatar/1.png",
+    img_condition: true,
+    content: "加客服回复加客服回复加客服回复加客服回复加客服回复入耳呜诶瑞热舞瑞沃尔UI一二的环境而",
+    reading: "91",
+    collection: "36",
+    postId : 3
+  },
+  {
+    date: "Sep 20 2015",
+    title: "张小龙谈微信",
+    imgSrc: "/images/post/xiaolong.jpg",
+    avatar: "/images/avatar/1.png",
+    img_condition: true,
+    content: "的时代仕杰的话说的话说的都是",
+    reading: "33",
+    collection: "60",
+    postId: 4
+  }
+];
+
+module.exports={
+  postList : local_database
+}
---
+++ pages/posts/post.js
@@ -0,0 +1,15 @@
+var postsData=require('../../data/posts-data.js');
+Page({
+  data: {
+  
+  },
+  onLoad: function (options) {
+    this.setData({ posts_key: postsData.postList})
+  },
+  onPostTap:function(event){
+    var postId=event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail'
+    })
+  }
+})
