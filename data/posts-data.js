c5030ad0f7eb8ad377a1f1795b1ac8169538990a
xushijie <15101587790@163.com>
2019-03-10 23:56:18 +0800

    xsj

--- data/posts-data.js
+++ data/posts-data.js
@@ -3,56 +3,76 @@ var local_database = [
     date: "Sep 18 2016",
     title: "正是虾肥蟹壮时",
     imgSrc: "/images/post/crab.png",
+    headImgSrc: "/images/post/crab.png",
     avatar: "/images/avatar/1.png",
+    author:"林白衣",
     img_condition: true,
     content: "看见很多风景都，是风华绝代放寒假快点放假，回家发的积分兑换积分的活但双方恢复对话，动仕杰方法见风使舵",
     reading: "112",
     collection: "96",
-    postId : 0
+    postId : 0,
+    dateTime : "24小时前",
+    detail: "额外未婚夫的货到付款发货的范登赫德发挥地方好地方额外未婚夫的货到付款发货的范登赫德发挥地方好地方额外未婚夫的货到付款发货的范登赫德发挥地方好地方额外未婚夫的货到付款发货的范登赫德发挥地方好地方额外未婚夫的货到付款发货的范登赫德发挥地方好地方"
   },
   {
     date: "Nov 21 2016",
     title: "比例点击就看到",
     imgSrc: "/images/post/bl.png",
+    headImgSrc: "/images/post/bl.png",
     avatar: "/images/avatar/1.png",
+    author:"王电话",
     img_condition: true,
     content: "诶入耳入额为为溽热UI为日俄乳热舞未入耳呜诶瑞热舞瑞沃尔UI一二的环境而",
     reading: "11",
     collection: "6",
-    postId: 1
+    postId: 1,
+    dateTime : "10小时前",
+    detail: "费电话费打客服电话对方回复对方的地方地方放到饭店对方回复的好看地方放到费电话费打客服电话对方回复对方的地方地方放到饭店对方回复的好看地方放到费电话费打客服电话对方回复对方的地方地方放到饭店对方回复的好看地方放到费电话费打客服电话对方回复对方的地方地方放到饭店对方回复的好看地方放到"
   },
   {
     date: "Sep 21 2016",
     title: "货到付款的发挥地方",
     imgSrc: "/images/post/cat.png",
+    headImgSrc:"/images/post/cat.png",
     avatar: "/images/avatar/1.png",
     img_condition: true,
     content: "江东父老就否定了记得发酒疯的发动机江东父老就否定了记得发酒疯的发动机江东父老就否定了记得发酒疯的发动机",
+    author: "王地方",
     reading: "121",
     collection: "699",
-    postId: 2
+    postId: 2,
+    dateTime: "3小时前",
+    detail: "对方回复的咖啡店发呆放大看对方的地方放到地方地对方回复的咖啡店发呆放大看对方的地方放到地方地方对方回复的咖啡店发呆放大看对方的地方放到地方地方对方回复的咖啡店发呆放大看对方的地方放到地方地方对方回复的咖啡店发呆放大看对方的地方放到地方地方对方回复的咖啡店发呆放大看对方的地方放到地方地方方"
   },
   {
     date: "Oct 11 2016",
     title: "sls的微信",
     imgSrc: "/images/post/sls.jpg",
+    headImgSrc: "/images/post/sls.jpg",
     avatar: "/images/avatar/1.png",
     img_condition: true,
+    author: "李地方",
     content: "加客服回复加客服回复加客服回复加客服回复加客服回复入耳呜诶瑞热舞瑞沃尔UI一二的环境而",
     reading: "91",
     collection: "36",
-    postId : 3
+    postId : 3,
+    dateTime: "19小时前",
+    detail: "额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃额度热度恶如热度呃呃"
   },
   {
     date: "Sep 20 2015",
     title: "张小龙谈微信",
     imgSrc: "/images/post/xiaolong.jpg",
+    headImgSrc: "/images/post/xiaolong.jpg",
     avatar: "/images/avatar/1.png",
     img_condition: true,
+    author: "李方",
     content: "的时代仕杰的话说的话说的都是",
     reading: "33",
     collection: "60",
-    postId: 4
+    postId: 4,
+    dateTime: "1小时前",
+    detail: "就地方放就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到就地方放到到"
   }
 ];
 
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,3 +1,44 @@
-Page({
+var postsData=require('../../../data/posts-data.js');
 
+Page({
+  data:{
+    currentPostId : ""
+  },
+  onLoad : function(option){
+    var postId=option.id;
+    this.setData({
+      currentPostId: postId
+    })
+    var postData = postsData.postList[postId];
+    this.setData({
+      postData: postData
+    })
+    var postsCollected = wx.getStorageSync("posts_collected");
+    if (postsCollected){
+      var postCollected = postsCollected[postId];
+      if (postCollected){
+        this.setData({
+          collected: postCollected
+        });
+      }
+    }else{
+      var postsCollected={};
+      postsCollected[postId]=false;
+      wx.setStorageSync("posts_collected", postsCollected);
+    }
+  },
+  onCollectionTap:function(event){
+    var postsCollected = wx.getStorageSync("posts_collected");
+    var postCollected = postsCollected[this.data.currentPostId];
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    wx.setStorageSync("posts_collected", postsCollected);
+    this.setData({
+      collected: postCollected
+    })
+    wx.showToast({
+      title: postCollected?"收藏成功":"取消成功",
+      duration : 1000  
+    })
+  }
 })
